1b943528e0038a47963c488198ef044d228112d8
Dongwoo Kang <41637359+DawsonKang@users.noreply.github.com>
2021-09-11 00:36:28 -0500

    working on re-design

--- src/components/members/Members.js
+++ src/components/members/Members.js
@@ -1,5 +1,6 @@
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
 
 //import css
 import "./members.scss"
@@ -10,35 +11,39 @@ export default function Members() {
     const data = [
         {
             img: "assets/emptyProfiles.png",
-            name: "Dongwoo Kang",
-            position: "backend",
-            bio: "testdofjsdifjsdijfidsjfodsjfkdsjf",
+            name: "Scott Song",
+            position: "Leader/Fronted",
+            bio: "This is fake bio and please do input something! ",
             linkedInURL: "https://linkedin.com/in/dongwookang0412",
             gitHubURL: "https://github.com/DawsonKang",
+            email: "",
+        },
+        {
+            img: "assets/emptyProfiles.png",
+            name: "Adam Carmody",
+            position: "Frontend",
+            bio: "This is fake bio and please do input something! ",
+            linkedInURL: "https://linkedin.com/in/dongwookang0412",
+            gitHubURL: "https://github.com/DawsonKang",
+            email: "",
+        },
+        {
+            img: "assets/emptyProfiles.png",
+            name: "Polina Semenova",
+            position: "Frontend",
+            bio: "This is fake bio and please do input something! ",
+            linkedInURL: "https://linkedin.com/in/dongwookang0412",
+            gitHubURL: "https://github.com/DawsonKang",
+            email: "",
         },
         {
             img: "assets/emptyProfiles.png",
             name: "Dongwoo Kang",
-            position: "backend",
-            bio: "testdofjsdifjsdijfidsjfodsjfkdsjf",
+            position: "Backend",
+            bio: "This is fake bio and please do input something! ",
             linkedInURL: "https://linkedin.com/in/dongwookang0412",
             gitHubURL: "https://github.com/DawsonKang",
-        },
-        {
-            img: "assets/emptyProfiles.png",
-            name: "Dongwoo Kang",
-            position: "backend",
-            bio: "testdofjsdifjsdijfidsjfodsjfkdsjf",
-            linkedInURL: "https://linkedin.com/in/dongwookang0412",
-            gitHubURL: "https://github.com/DawsonKang",
-        },
-        {
-            img: "assets/emptyProfiles.png",
-            name: "Dongwoo Kang",
-            position: "backend",
-            bio: "testdofjsdifjsdijfidsjfodsjfkdsjf",
-            linkedInURL: "https://linkedin.com/in/dongwookang0412",
-            gitHubURL: "https://github.com/DawsonKang",
+            email: "dawson41271@gmail.com",
         },
     ]
 
@@ -54,15 +59,20 @@ export default function Members() {
                         <div className="wrapper">
                             <h1>
                                 {member.name}
+                                <a target="_blank" rel="noopener noreferrer" href={member.email}>
+                                    <EmailIcon/>
+                                </a>
                                 <a target="_blank" rel="noopener noreferrer" href={member.gitHubURL}>
-                                    <GitHubIcon style={{padding: "2px"}}/>
+                                    <GitHubIcon/>
                                 </a>
                                 <a target="_blank" rel="noopener noreferrer" href={member.linkedInURL}>
-                                    <LinkedInIcon style={{padding: "2px"}}/>
+                                    <LinkedInIcon/>
                                 </a>
                             </h1>
                             <h2>{member.position}</h2>
-                            <h3>{member.bio}</h3>
+                            <div className="bio">
+                                <h3>{member.bio}</h3>
+                            </div>
                         </div>
                     </div>
                 ))}
