e3447524d4d8076c0dcfa37970c3826b83449dec
Dongwoo Kang <41637359+DawsonKang@users.noreply.github.com>
2021-09-11 22:30:40 -0500

    .

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -80,16 +80,13 @@ export default function Home() {
                     <div className="wrapper">
                         <h2>Who Are We?</h2>
                         <h1>&nbsp; &nbsp; &nbsp; &nbsp;<span ref={textRef}></span></h1>
-                        <h3>&nbsp;&nbsp;Simple Sentence</h3>
+                        <h3>&nbsp;&nbsp;"Let's Change the World!"</h3>
                         <p>
-                        “Mary Jones is an Administrative Assistant with eight years of experience 
+                        Dummy: Mary Jones is an Administrative Assistant with eight years of experience 
                         working alongside the executive team of a Fortune 500 company. 
                         Mary specializes in administrative technology and is responsible for educating 
                         other employees on using progressive systems and applications, including accounting software, 
-                        mass communication procedures and organizational apps. Mary is a powerful force in the workplace 
-                        and uses her positive attitude and tireless energy to encourage others to work hard and succeed. 
-                        Mary is inspired daily by her husband and their two daughters. In her free time, Mary likes to hike,
-                        crochet and play video games with her grandson."
+                        mass communication procedures and organizational apps. 
                         </p>
                     </div>
                 </div>
